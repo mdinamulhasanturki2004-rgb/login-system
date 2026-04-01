c1ddb903054142c3d284b203f83bf70f1c6c1041
mdinamulhasanturki2004-rgb <mdinamulhasanturki2004@gmail.com>
2026-04-02 00:33:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+// ===== SIGNUP =====
+function signup() {
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username === "" || password === ""){
+        alert("Fill all fields");
+        return;
+    }
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    alert("Signup Successful ✅");
+
+    window.location.href = "index.html";
+}
+
+
+// ===== LOGIN =====
 function login(){
+
     let user = document.getElementById("loginUser").value;
     let pass = document.getElementById("loginPass").value;
 
@@ -10,21 +32,31 @@ function login(){
         localStorage.setItem("loggedInUser", user);
 
         alert("Login Successful ✅");
-        window.location.href="dashboard.html";
+        window.location.href = "dashboard.html";
 
     }else{
         alert("Wrong Username or Password ❌");
     }
 }
 
-// Show username on dashboard
+
+// ===== SHOW USER ON DASHBOARD =====
 if(window.location.pathname.includes("dashboard.html")){
-    let user = localStorage.getItem("loggedInUser");
-    document.getElementById("welcomeUser").innerText =
-        "Hello, " + user + " 👋";
+
+    let loggedUser = localStorage.getItem("loggedInUser");
+
+    if(!loggedUser){
+        alert("Please login first");
+        window.location.href = "index.html";
+    }else{
+        document.getElementById("welcomeUser").innerText =
+        "Welcome, " + loggedUser + " 👋";
+    }
 }
 
+
+// ===== LOGOUT =====
 function logout(){
     localStorage.removeItem("loggedInUser");
-    window.location.href="index.html";
+    window.location.href = "index.html";
 }
